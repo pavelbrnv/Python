7cb1f0b92fc6eb33f1be7bae508304df5b3db0b5
Baranov Pavel <pavel.brnv@gmail.com>
2022-04-08 00:21:46 +0300

    Methods for opti searching

--- Percents/deposit.py
+++ Percents/deposit.py
@@ -1,4 +1,5 @@
 from dateutil.relativedelta import relativedelta
+from datetime import date
 
 
 def round_finance(value):
@@ -6,15 +7,10 @@ def round_finance(value):
 
 
 class PayInfo:
-    def __init__(self, index, pay_date, pay_amount, pays_total_amount, deposit_total_amount):
-        self.__index = index
+    def __init__(self, pay_date, pay_amount, deposit_total_amount):
         self.__pay_date = pay_date
         self.__pay_amount = pay_amount
-        self.__pays_total_amount = pays_total_amount
         self.__deposit_total_amount = deposit_total_amount
-
-    def get_index(self):
-        return self.__index
 
     def get_pay_date(self):
         return self.__pay_date
@@ -22,20 +18,17 @@ class PayInfo:
     def get_pay_amount(self):
         return self.__pay_amount
 
-    def get_pays_total_amount(self):
-        return self.__pays_total_amount
-
     def get_deposit_total_amount(self):
         return self.__deposit_total_amount
 
 
 class Deposit:
-    def __init__(self, amount, percent_per_year, deposit_date):
+    def __init__(self, amount: float, percent_per_year: float, deposit_date: date):
         self.__amount = amount
         self.__percent_per_year = percent_per_year / 100
         self.__deposit_date = deposit_date
 
-    def get_total_amount(self, term_in_months):
+    def get_total_amount(self, term_in_months: int):
         percent_per_month = self.__percent_per_year / 12
         total_amount = self.__amount
         for month_index in range(term_in_months):
@@ -43,12 +36,12 @@ class Deposit:
             total_amount = round_finance(total_amount + pay_amount)
         return total_amount
 
-    def get_total_profit(self, term_in_months):
+    def get_total_profit(self, term_in_months: int):
         total_amount = self.get_total_amount(term_in_months)
         total_profit = total_amount - self.__amount
         return round_finance(total_profit)
 
-    def get_pays(self, term_in_months):
+    def get_pays(self, term_in_months: int, month_additional_refill: float = 0):
         percent_per_month = self.__percent_per_year / 12
         total_amount = self.__amount
         pay_date = self.__deposit_date
@@ -56,8 +49,7 @@ class Deposit:
         for month_index in range(term_in_months):
             pay_date += relativedelta(months=1)
             pay_amount = round_finance(total_amount * percent_per_month)
-            total_amount = round_finance(total_amount + pay_amount)
-            pays_total_amount = round_finance(total_amount - self.__amount)
-            pay = PayInfo(month_index + 1, pay_date, pay_amount, pays_total_amount, total_amount)
+            total_amount = round_finance(total_amount + pay_amount + month_additional_refill)
+            pay = PayInfo(pay_date, pay_amount, total_amount)
             pays.append(pay)
         return pays
--- Percents/main.py
+++ Percents/main.py
@@ -28,42 +28,85 @@ def print_all_fees(fees: list[annuity.FeeInfo]):
         )
         total_fees += fee.get_fee_amount()
         index += 1
-    print("---", "TOTAL FEES: ", total_fees, "---")
+    print("---", "TOTAL FEES:", total_fees, "---")
 
 
 def print_pay(pay: deposit.PayInfo):
     print(
-        pay.get_index(),
         pay.get_pay_date(),
         pay.get_pay_amount(),
-        pay.get_pays_total_amount(),
         pay.get_deposit_total_amount()
     )
 
 
-loan1 = annuity.Loan(
+def print_all_pays(pays: list[deposit.PayInfo]):
+    index = 1
+    total_pays = 0
+    for pay in pays:
+        print(
+            index,
+            pay.get_pay_date(),
+            pay.get_pay_amount(),
+            pay.get_deposit_total_amount()
+        )
+        total_pays += pay.get_pay_amount()
+        index += 1
+    total_refill = pays[-1].get_deposit_total_amount() - total_pays
+    print("---", "TOTAL PAYS:", total_pays, "---", "TOTAL REFILL:", total_refill, "---")
+
+
+def find_opti(fees: list[annuity.FeeInfo], pays: list[deposit.PayInfo]):
+    fee_i = 0
+    pay_i = 0
+
+    while fees[fee_i].get_debt_total_remainder() > pays[pay_i].get_deposit_total_amount():
+        if fees[fee_i].get_fee_date() <= pays[pay_i].get_pay_date():
+            fee_i += 1
+            if fee_i == len(fees):
+                break
+        else:
+            pay_i += 1
+            if pay_i == len(pays):
+                break
+
+    if fee_i == len(fees) or pay_i == len(pays):
+        print('Sorry :( It is not your day')
+    else:
+        print_fee(fees[fee_i])
+        print_pay(pays[pay_i])
+
+
+loan = annuity.Loan(
     amount=6422721,
     percent_per_year=6.1,
     term_in_months=223,
-    loan_date=date(2022, 3, 19)
+    loan_date=date(2022, 4, 7)
 )
-loan1.add_periodical_early_fees(
-    first_fee_date=date(2022, 4, 15),
-    fee_amount=100000,
-    periods_delta=relativedelta(months=1),
-    periods_number=240
-)
+# loan = annuity.Loan(
+#     amount=6000000,
+#     percent_per_year=6.1,
+#     term_in_months=198,
+#     loan_date=date(2022, 4, 7)
+# )
+# loan.add_periodical_early_fees(
+#     first_fee_date=date(2022, 4, 15),
+#     fee_amount=150000,
+#     periods_delta=relativedelta(months=1),
+#     periods_number=240
+# )
 
 deposit = deposit.Deposit(
-    amount=50000,
-    percent_per_year=21,
-    deposit_date=date(2022, 4, 5)
+    amount=500000,
+    percent_per_year=12,
+    deposit_date=date(2022, 4, 7)
 )
 
-loan_fees = loan1.get_annuity_daily_fees()
+loan_fees = loan.get_annuity_daily_fees()
+deposit_pays = deposit.get_pays(term_in_months=60, month_additional_refill=150000)
+
+find_opti(loan_fees, deposit_pays)
+
+print()
+
 print_all_fees(loan_fees)
-
-# deposit_pays = deposit.get_pays(term_in_months=3)
-
-# for pay in deposit_pays:
-#     print_pay(pay)
+#print_all_pays(deposit_pays)
